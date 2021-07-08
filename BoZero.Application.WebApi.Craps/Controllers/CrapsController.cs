b2bc793ccfe0608ef7730c0d5fd765239ae48e0e
Daniel Foster <bozero1974@gmail.com>
2021-07-08 15:20:24 -0400

    Changes

---
+++ BoZero.Application.WebApi.Craps/Controllers/CrapsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoZero.Application.WebApi.Craps.Models;
+
+namespace BoZero.Application.WebApi.Craps.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CrapsController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetDice()
+		{
+			var rng = new Random();
+			//var roll = new {Die1 = rng.Next(6) + 1, Die2 = rng.Next(6) + 1 };
+			var rollModel = new RollModel {Die1 = rng.Next(6) + 1, Die2 = rng.Next(6) + 1};
+			return Ok(rollModel);
+		}
+	}
+}
---
+++ BoZero.Application.WebApi.Craps/Models/RollModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace BoZero.Application.WebApi.Craps.Models
+{
+	public class RollModel
+	{
+		public int Die1 { get; set; }
+		
+		public int Die2 { get; set; }
+	}
+}
