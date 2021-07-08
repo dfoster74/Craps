bf0fcf64ca839cc41d3b5046799d77f25c7c970d
Daniel Foster <bozero1974@gmail.com>
2021-07-08 16:45:28 -0400

    Added RandomNumberService

---
+++ BoZero.Craps.Business.Core/Services/RandomNumberService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using BoZero.Craps.Business.Core.Interfaces;
+
+namespace BoZero.Craps.Business.Core.Services
+{
+	public class RandomNumberService : IRandomNumberService
+	{
+		private readonly Random _rng = new Random();
+
+		public int GetRandomNumber(int maxValue)
+		{
+			return _rng.Next(maxValue);
+		}
+	}
+}
--- BoZero.Craps.Business.Core/Services/RollService.cs
+++ BoZero.Craps.Business.Core/Services/RollService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using BoZero.Craps.Business.Core.Entities;
 using BoZero.Craps.Business.Core.Interfaces;
 
@@ -6,11 +7,17 @@ namespace BoZero.Craps.Business.Core.Services
 {
 	public class RollService : IRollService
 	{
+		private readonly IRandomNumberService _randomNumberService;
+
+		public RollService(IRandomNumberService randomNumberService)
+		{
+			_randomNumberService = randomNumberService;
+		}
+
 		public Roll GetRoll()
 		{
-			var rng = new Random();
-			var die1 = rng.Next(6) + 1;
-			var die2 = rng.Next(6) + 1;
+			var die1 = _randomNumberService.GetRandomNumber(6) + 1;
+			var die2 = _randomNumberService.GetRandomNumber(6) + 1;
 			return new Roll(die1, die2);
 		}
 	}
